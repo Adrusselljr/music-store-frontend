539316bdec12054eef7f9596637817cdd7192c19
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-06-02 11:08:05 -0400

    initial setup

---
+++ client/src/componenets/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+function Layout() {
+    return (
+        <div>
+        
+            <h1>Layout</h1>
+
+        </div>
+    )
+}
+
+export default Layout
