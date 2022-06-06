d2b8f3c24102fb73d8f17eca87a3a3b14c8f4201
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-06-06 15:16:16 -0400

    CREATED: ProductDisplay/dataFetching MODIFIED: App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import Layout from './componenets/Layout'
+import HomePage from './pages/HomePage'
 import CustomThemeProvider from './componenets/CustomThemeProvider'
 
 function App() {
-    // return "hi"
     return (
         <div>
-
             <CustomThemeProvider>
-                <Layout>
-                    Home Page
-                </Layout>
+                <HomePage />
             </CustomThemeProvider>
-        
         </div>
     )
 }
