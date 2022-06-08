b60d18abedbb37580c00ab90dc9674cd465ca631
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-06-08 15:21:21 -0400

    CREATED CartItem component

---
+++ client/src/componenets/CartItem.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { Card, Box, CardMedia, Typography, IconButton } from '@mui/material'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { shoppingCartContext } from '../context/shoppingCartContext';
+
+const CartItem = props => {
+    const { cartItem } = props
+    const { removeFromCart } = useContext(shoppingCartContext)
+
+    return (
+        <Card>
+            <Box display="flex">
+                <Box>
+                    <CardMedia component="img" sx={{ width: 50, height: 50, p: 1 }} image={ cartItem.image }/>
+                </Box>
+                <Box px={ 2 } display="flex" flexDirection="column" flexGrow={ 1 } justifyContent="center">
+                    <Box>
+                        <Typography fontWeight="bold">
+                            { cartItem.title }
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography fontWeight="bold">
+                            ${ cartItem.price / 100 }
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box px={ 2 } display="flex" flexDirection="column" justifyContent="center">
+                    <Typography fontWeight="bold">
+                            x{ cartItem.quantity }
+                    </Typography>
+                </Box>
+                <Box px={ 2 } display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                    <IconButton onClick={ () => removeFromCart(cartItem.id) }>
+                        <DeleteForeverIcon/>
+                    </IconButton>
+                </Box>
+            </Box>
+        </Card>
+    )
+}
+
+export default CartItem
--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,22 +1,18 @@
 import React, { useContext } from 'react';
 import { shoppingCartContext } from '../context/shoppingCartContext';
-import { Box, Button } from '@mui/material';
+import CartItem from '../componenets/CartItem';
+import { Box } from '@mui/material';
 import Layout from '../componenets/Layout';
 
 const CartPage = () => {
-    const { shoppingCart, removeFromCart } = useContext(shoppingCartContext)
+    const { shoppingCart } = useContext(shoppingCartContext)
 
     return (
         <Layout shoppingCart={ shoppingCart }>
             <Box width={ 1 } display="flex" flexDirection="column" alignItems="center">
                 {shoppingCart.map(cartItem =>
-                    <Box key={ cartItem.id } p={ 3 }>
-                        <Box>
-                            { cartItem.title } - Qty: { cartItem.quantity } - ${ cartItem.price / 100 } total: ${ cartItem.total / 100 }
-                        </Box>        
-                        <Box>
-                            <Button variant='outlined' onClick={ () => removeFromCart(cartItem.id) } >Remove From cart</Button>
-                        </Box>
+                    <Box key={ cartItem.id } p={ 3 } width="100%" maxWidth="500px">
+                        <CartItem cartItem={ cartItem } />
                     </Box>
                 )}
             </Box>
