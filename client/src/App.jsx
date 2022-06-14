692a666fd5a2db5708864a56bc952d61337bd5cc
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-06-14 10:33:18 -0400

    added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,27 @@
-import React, { useState} from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CartPage from './pages/CartPage';
 import HomePage from './pages/HomePage';
 import CustomThemeProvider from './componenets/CustomThemeProvider';
 import ShoppingCartProvidor from './context/shoppingCartContext';
 import UserDataProvider from './context/userDataContext';
 import SignInPage from './pages/SignInPage';
-import { Box, Button } from '@mui/material';
 
 const App = () => {
-    const [page, setPage] = useState('homePage')
-
-    const getPage = () => {
-        switch (page) {
-            case 'cartPage':
-                return <CartPage />
-            case 'signInPage':
-                return <SignInPage />
-            default:
-                return <HomePage />
-        }
-    }
-
     return (
-        <CustomThemeProvider>
-            <UserDataProvider>
-                <ShoppingCartProvidor>
-                    <Box m={ 1 } display='flex' justifyContent='space-around'>
-                        <Button variant='contained' type="button" onClick={ () => setPage('homePage') }>Home Page</Button>
-                        <Button variant='contained' type="button" onClick={ () => setPage('cartPage') }>Cart Page</Button>
-                        <Button variant='contained' type="button" onClick={ () => setPage('signInPage') }>Sign In Page</Button>
-                    </Box>
-                    {getPage()}
-                </ShoppingCartProvidor>
-            </UserDataProvider>
-        </CustomThemeProvider>
+        <BrowserRouter>
+            <CustomThemeProvider>
+                <UserDataProvider>
+                    <ShoppingCartProvidor>
+                        <Routes>
+                            <Route path="/" element={ <HomePage /> } />
+                            <Route path="/sign-in" element={ <SignInPage /> } />
+                            <Route path="/cart" element={ <CartPage /> } />
+                        </Routes>
+                    </ShoppingCartProvidor>
+                </UserDataProvider>
+            </CustomThemeProvider>
+        </BrowserRouter>
     )
 }
 
--- client/src/componenets/Header.jsx
+++ client/src/componenets/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useShoppingCart } from '../context/shoppingCartContext';
 import { useUserData } from '../context/userDataContext'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { AppBar, Badge, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import '../index.css';
 
 const Header = () => {
     const { shoppingCart } = useShoppingCart()
@@ -16,25 +18,33 @@ const Header = () => {
     return (
         <AppBar position="static" color="secondary">
             <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                My Music Store
-            </Typography>
-            <Box mr={ 2 }>
-                {
-                    user === undefined
-                    ? <Button variant="contained" color="primary">Sign In</Button>
-                    : <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>Hi, { user.firstName }</Typography>
-                }
-            </Box>
-            <IconButton
-                size="large"
-                aria-label="Go to shopping cart"
-                color="inherit"
+                <Box sx={{ flexGrow: 1 }}>
+                    <Link to="/">
+                        <Typography variant="h6" component="div">
+                            My Music Store
+                        </Typography>
+                    </Link>
+                </Box>
+                <Box mr={ 2 }>
+                    {
+                        user === undefined
+                        ? <Link to="/sign-in"><Button variant="contained" color="primary">Sign In</Button></Link>
+                        : <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>Hi, { user.firstName }</Typography>
+                    }
+                </Box>
+                <IconButton
+                    size="large"
+                    aria-label="Go to shopping cart"
+                    color="inherit"
                 >
-                <Badge badgeContent={ itemQuantity } color="primary">
-                    <ShoppingCartIcon />
-                </Badge>
-            </IconButton>
+                    <Box mr={ 2 }>
+                        <Link to="/cart">
+                            <Badge badgeContent={ itemQuantity } color="primary">
+                                <ShoppingCartIcon />
+                            </Badge>
+                        </Link>
+                    </Box>
+                </IconButton>
             </Toolbar>
         </AppBar>
     )
